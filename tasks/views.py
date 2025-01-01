cdd0fdd7b33b8eceed70017fba53a6629f68e47c
Josue_Klaysler <josueklayslerjk@gmail.com>
2025-01-01 20:29:14 -0300

    Add task list and add task views for managing tasks

--- tasks/views.py
+++ tasks/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from .models import Task
 
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        Task.objects.create(title=title)
+        description = request.POST.get('description')
+        Task.objects.create(title=title, description=description)
+        return redirect('task_list')
+    return render(request, 'tasks/add_task.html')
