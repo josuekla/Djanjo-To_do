a8d3c79ee64dd2411600fc473833cad2f0ba52d7
Josue_Klaysler <josueklayslerjk@gmail.com>
2025-01-03 21:47:58 -0300

    Add the url of update and delete in task/views.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add/', views.add_task, name='add_task'),
+    path('update/<int:pk>/', views.update_task, name='update_task'),
+    path('delete/<int:pk>/', views.delete_task, name='delete_task'),
 ]
